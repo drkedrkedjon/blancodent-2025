a7b2fc5447175f27b26ce6486961ac61f1f57378
Sasha S.P. <sasa@bubulazy.com>
2024-11-13 12:18:20 +0000

    Defined oklich color palete and hsl fallbacks

--- src/app/components/header/Header/Header.tsx
+++ src/app/components/header/Header/Header.tsx
@@ -2,6 +2,12 @@ export default function Header() {
   return (
     <header>
       <h1>Header</h1>
+      <div className="test">1</div>
+      <div className="test">2</div>
+      <div className="test">3</div>
+      <div className="test">1</div>
+      <div className="test">2</div>
+      <div className="test">1</div>
     </header>
   );
 }
