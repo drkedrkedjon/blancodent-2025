78702eed9709b99f2016ef698f981d8b424b0970
Sasha S.P. <sasa@bubulazy.com>
2024-11-13 12:43:06 +0000

    Checking color contrast for accessibility

--- src/app/components/header/Header/Header.tsx
+++ src/app/components/header/Header/Header.tsx
@@ -7,7 +7,9 @@ export default function Header() {
       <div className="test">3</div>
       <div className="test">1</div>
       <div className="test">2</div>
-      <div className="test">1</div>
+      <div className="test">A</div>
+      <div className="test">B</div>
+      <div className="test">W</div>
     </header>
   );
 }
