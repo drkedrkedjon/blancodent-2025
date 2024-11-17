129645225054b78b7b73eca03544316c36b9e1a2
Sasha S.P. <sasa@bubulazy.com>
2024-11-17 19:25:27 +0000

    Playing with color palete and accessibility with color contrast-still...

--- src/app/components/header/Header/Header.tsx
+++ src/app/components/header/Header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={"test"}>
+      <h1>H</h1>
       <div className="test">1</div>
       <div className="test">2</div>
       <div className="test">3</div>
