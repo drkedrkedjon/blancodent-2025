547c9a06e7a150b7484da4927b490491c540f6e4
Sasha S.P. <sasa@bubulazy.com>
2024-11-18 19:05:48 +0000

    Implemented grid classes for layout. The one that uses grid line names. Still I have to fix some details

--- src/app/components/header/Header/Header.tsx
+++ src/app/components/header/Header/Header.tsx
@@ -1,7 +1,21 @@
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="layout-grid">
+      <h1>Header de cojones que esta de cojones</h1>
+      <h1 className="narrow">Header de cojones que esta de cojones</h1>
+      <h1>Header de cojones que esta de cojones</h1>
+      <h1>Header de cojones que esta de cojones</h1>
+      <h1 className="narrow">
+        Header de cojones que esta de cojones skjdfhsdhf
+      </h1>
+      <h1>Header de cojones que esta de cojones</h1>
+      <div className="wide-width">
+        <h1>1 Header de cojones que esta de cojones</h1>
+        <h1 className="narrow">Header de cojones que esta de cojones</h1>
+      </div>
+      <h1>Header de cojones que esta de cojones</h1>
+      <h1>Header de cojones que esta de cojones</h1>
+      <h1>Header de cojones que esta de cojones</h1>
     </header>
   );
 }
