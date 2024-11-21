4925e4a1780f8d4c3032e4356bcfd22390ab1fee
Sasha S.P. <sasa@bubulazy.com>
2024-11-21 12:51:40 +0000

    Working on Language Switcher Component, messages done.

--- src/app/components/header/LangSwitcher/LangSwitcher.tsx
+++ src/app/components/header/LangSwitcher/LangSwitcher.tsx
@@ -1,6 +1,10 @@
 import LangSwitcherSelect from "@/app/components/header/LangSwitcherSelect";
+import { useLocale, useTranslations } from "next-intl";
 
 export default function LangSwitcher() {
+  const t = useTranslations("LangSwitcher");
+  const locale = useLocale();
+
   return (
     <LangSwitcherSelect>
       <option value="en">English</option>
