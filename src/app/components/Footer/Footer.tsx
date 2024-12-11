e9b379a6c725afd09dc813688b5dd267c908adc6
Sasha S.P. <sasa@bubulazy.com>
2024-12-11 13:47:10 +0000

    Most of footer done, mailchimp suscribe btn done

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,101 +6,109 @@ export default function Footer() {
   return (
     <footer className={`${styles.footer} wide-grid `}>
       <div className={styles.cardsContainer}>
-        <nav
-          className={styles.navLinks}
-          aria-labelledby="nav-label"
-        >
-          <h2 id="nav-label">Enlaces</h2>
-          <ul role="list">
-            <li>
-              <Link href="/">Página de inicio</Link>
-            </li>
-            <li>
-              <Link href="/varios/preguntas-frecuentes">
-                Preguntas frecuentes
-              </Link>
-            </li>
-            <li>
-              <Link href="/varios/ofertas-continuas">Últimas ofertas</Link>
-            </li>
-            <li>
-              <Link href="/contacto">Contacto</Link>
-            </li>
-            <li>
-              <Link href="tel:+34928292455">Llámanos</Link>
-            </li>
-          </ul>
-        </nav>
-        <div>
-          <h2>Horarios</h2>
-          <p>
-            <strong>Lunes:</strong>
-            <br />
-            09:30 - 16:00.
-          </p>
-          <p>
-            <strong>Martes:</strong>
-            <br />
-            09:30 - 16:00.
-          </p>
-          <p>
-            <strong>Miércoles:</strong>
-            <br />
-            09:30 - 16:00.
-          </p>
-          <p>
-            <strong>Jueves:</strong>
-            <br />
-            09:30 - 16:00.
-          </p>
-          <p>
-            <strong>Viernes:</strong>
-            <br />
-            09:30 - 16:00.
-          </p>
-        </div>
-        <address>
-          <h2>Dirección</h2>
-          <p>
-            Av. Mesa y López, 1 - 1ª izq.
-            <br />
-            35006, Las Palmas de G.C.
-            <br />
-            Islas Canarias, España.
-          </p>
-          <p>
-            <strong>Redes sociales:</strong>
-          </p>
-          <ul
-            className={styles.socialNetworks}
-            role="list"
+        <div className={styles.boxOne}>
+          <nav
+            className={styles.navLinks}
+            aria-labelledby="nav-label"
           >
-            <li>
-              <Link href="https://www.facebook.com/">
-                <FacebookLogo weight="duotone" />
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.instagram.com/">
-                <InstagramLogo weight="duotone" />
-              </Link>
-            </li>
-          </ul>
-        </address>
-        <div>
-          <h2>Boletín</h2>
-          <p>
-            Suscríbete a nuestro boletín para recibir ofertas y descuentos
-            exclusivos tres veces al año.
-          </p>
-          <form>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-            />
-            <button type="submit">Suscribirme</button>
-          </form>
+            <h2 id="nav-label">Enlaces</h2>
+            <ul role="list">
+              <li>
+                <Link href="/">Página de inicio</Link>
+              </li>
+              <li>
+                <Link href="/varios/preguntas-frecuentes">
+                  Preguntas frecuentes
+                </Link>
+              </li>
+              <li>
+                <Link href="/varios/ofertas-continuas">Últimas ofertas</Link>
+              </li>
+              <li>
+                <Link href="/contacto">Contacto</Link>
+              </li>
+              <li>
+                <Link href="tel:+34928292455">Llámanos</Link>
+              </li>
+            </ul>
+          </nav>
+          <address>
+            <h2>Dirección</h2>
+            <p>
+              Av. Mesa y López, 1 - 1ª izq.
+              <br />
+              35006, Las Palmas de G.C.
+              <br />
+              Islas Canarias, España.
+            </p>
+            <p>
+              <strong>Redes sociales:</strong>
+            </p>
+            <ul
+              className={styles.socialNetworks}
+              role="list"
+            >
+              <li>
+                <Link
+                  target="_blank"
+                  href="https://www.facebook.com/clinica.dental.blancodent.las.palmas/"
+                >
+                  <FacebookLogo weight="duotone" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  target="_blank"
+                  href="https://www.instagram.com/blancodentclinicadental_gc/"
+                >
+                  <InstagramLogo weight="duotone" />
+                </Link>
+              </li>
+            </ul>
+          </address>
+        </div>
+        <div className={styles.boxTwo}>
+          <div>
+            <h2>Horarios</h2>
+            <p>
+              <strong>Lunes:</strong>
+              <br />
+              09:30 - 16:00.
+            </p>
+            <p>
+              <strong>Martes:</strong>
+              <br />
+              14:00 - 20:00.
+            </p>
+            <p>
+              <strong>Miércoles:</strong>
+              <br />
+              09:30 - 16:00.
+            </p>
+            <p>
+              <strong>Jueves:</strong>
+              <br />
+              14:00 - 20:00.
+            </p>
+            <p>
+              <strong>Viernes:</strong>
+              <br />
+              09:30 - 16:00.
+            </p>
+          </div>
+          <div>
+            <h2>Newsletter</h2>
+            <p>
+              Suscríbete a nuestro newsletter para recibir ofertas y descuentos
+              exclusivos no mas que cuatro veces al año.
+            </p>
+            <Link
+              className="btnLink"
+              href="https://mailchi.mp/blancodent.com/blancodent-web"
+            >
+              Suscribirse
+            </Link>
+          </div>
         </div>
       </div>
       <div className={`narrow-grid ${styles.notaLegalContainer}`}>
