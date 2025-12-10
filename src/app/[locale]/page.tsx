247866cad787dda043b1d13f6144218536af4751
Sasha S.P. <sasa@bubulazy.com>
2025-12-10 18:07:18 +0100

    Fineished Offers component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,6 +14,7 @@ export default async function HomePage({
   params: Promise<{ locale: string }>;
 }>) {
   const { locale } = await params;
+
   // Enable static rendering in next-intl
   setRequestLocale(locale);
   const t = await getTranslations("HomePage");
