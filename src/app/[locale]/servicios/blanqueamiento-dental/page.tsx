59feea31a719041e514ea38582c76090a71a4602
Sasha S.P. <sasa@bubulazy.com>
2025-12-23 10:30:27 +0100

    Changed prototype for articles to use layout again and changed aside to images

--- src/app/[locale]/servicios/blanqueamiento-dental/page.tsx
+++ src/app/[locale]/servicios/blanqueamiento-dental/page.tsx
@@ -1,5 +1,10 @@
+import recepcion from "@/assets/images/home-page/blanqueamiento.jpg";
+import personal from "@/assets/images/home-page/conservadora.jpg";
+import noSeQue from "@/assets/images/home-page/imagenes.jpg";
+import consulta from "@/assets/images/home-page/ortodoncia.jpg";
 import { routing } from "@/i18n/routing";
 import { hasLocale } from "next-intl";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 import styles from "./page.module.css";
 
@@ -33,7 +38,27 @@ export default async function BlanqueamientoDental({
         <Content />
       </div>
       <aside className={styles.aside}>
-        <h3>Implants</h3>
+        {/* <h3>Recepcion</h3> */}
+        <Image
+          src={recepcion}
+          alt=""
+        />
+        {/* <h3>Personal</h3> */}
+        <Image
+          src={personal}
+          alt=""
+        />
+        {/* <h3>Consulta</h3> */}
+        <Image
+          src={consulta}
+          alt=""
+        />
+        {/* <h3>Consulta</h3> */}
+        <Image
+          src={noSeQue}
+          alt=""
+        />
+        {/* <h3>Implants</h3>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sint
           omnis tempore enim voluptatibus tempora laborum! Nostrum atque
@@ -56,7 +81,7 @@ export default async function BlanqueamientoDental({
           temporibus adipisci obcaecati quos doloribus quisquam aliquam ab
           beatae tempore qui est atque impedit nesciunt minima dignissimos
           facere voluptates?
-        </p>
+        </p> */}
       </aside>
     </article>
   );
