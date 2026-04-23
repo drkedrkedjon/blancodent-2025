c35e99f702e385f8f41a45f1c286c0fa1677d339
Sasha S.P. <sasa@bubulazy.com>
2026-04-23 15:55:09 +0200

    Work on content in asi somos

---
+++ src/app/[locale]/asi-somos/financiacion/page.tsx
@@ -0,0 +1,49 @@
+import recepcion from "@/assets/images/home-page/blanqueamiento.jpg";
+import personal from "@/assets/images/home-page/conservadora.jpg";
+import noSeQue from "@/assets/images/home-page/imagenes.jpg";
+import consulta from "@/assets/images/home-page/ortodoncia.jpg";
+import AsideImageBlock from "@/components/AsideImageBlock/AsideImageBlock";
+import { routing } from "@/i18n/routing";
+import { hasLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import styles from "../asi-somos.module.css";
+interface ServiciosLayoutProps {
+  params: Promise<{ locale: string }>;
+}
+
+export default async function BlanqueamientoDental({
+  params,
+}: ServiciosLayoutProps) {
+  // Ensure that the incoming `locale` is valid
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  // Dynamic import based on locale
+  const { metadata, default: Content } = await import(
+    locale && `./markdown/content-${locale}.mdx`
+  );
+
+  return (
+    <article className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.h1}>{metadata?.title}</h1>
+        <p className={styles.date}>Última edición: {metadata?.date}</p>
+        <p className={styles.subTitle}>{metadata?.subTitle}</p>
+      </header>
+      <div className={styles.line} />
+      <div className={styles.content}>
+        <Content />
+      </div>
+      <div className={styles.asideContainer}>
+        <AsideImageBlock
+          url1={recepcion}
+          url2={personal}
+          url3={noSeQue}
+          url4={consulta}
+        />
+      </div>
+    </article>
+  );
+}
